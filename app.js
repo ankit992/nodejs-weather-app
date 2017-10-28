79abd805018ef068828a796b6bb034700528e90b
Ankit Arora <ankit992@live.in>
2017-10-28 22:34:37 +0530

    Adding error handling in the request

--- app.js
+++ app.js
@@ -14,12 +14,20 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
+var encodedAddress = encodeURIComponent(argv.address);
 request({ 
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=Rajiv%20Nagar%20Gurgaon',
+    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true 
     }, 
     (error, response, body) => {
-        console.log(`Address: ${body.results[0].formatted_address}`)
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        if(error) {
+            console.log('Unable to connect to google servers.');
+        }
+        else if (body.status === 'ZERO_RESULTS'){
+            console.log('Unable to find that address');
+        } else if (body.status === 'OK'){
+            console.log(`Address: ${body.results[0].formatted_address}`)
+            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        }
 });
